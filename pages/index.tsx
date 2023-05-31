f09b1fdf5ed2dcae8df2bde4d004cc5fa2d59515
MattGomb <gombosonline@gmail.com>
2023-05-30 21:42:10 -0500

    Create footer and update structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +13,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        className={`flex min-h-screen flex-col items-center justify-between px-24 py-6 ${inter.className}`}
       >
         <div>hello world</div>
         <h3>great job running next js</h3>
+      <Footer />
       </main>
     </>
   );
